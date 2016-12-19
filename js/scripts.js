e020fb8bdfa0cf714d7fd81602b5dfc7456e31a3
vladimir <vovmaster2@gmail.com>
2016-12-19 06:22:42 +0200

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,3 @@
 $(document).ready(function() {
-	if($('body').width() > 767) {
-		retinajs(document.querySelectorAll('.table-retina'));
-	}
-	retinajs(document.querySelectorAll('*[data-rjs]:not(.table-retina)'));
 
-	$('.slider').slick({
-	  dots: true,
-	  infinite: true,
-	  speed: 500
-	});
-   $(".fancybox").fancybox({
-   	padding: 0
-   });
-   $('.button-menu').click(function(){
-   	$(this).toggleClass('open-nav');
-   });
-
-   setTimeout(function() {
-		$('.wrap-opacity-block').fadeOut();
-   }, 10000);
-
-
-   $('.wrap-opacity-block').click(function(){
-   		$('.wrap-opacity-block').fadeOut();
-   });
 });
